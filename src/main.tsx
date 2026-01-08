ccb0b2f37aee88cd923eb8d778d8b814ce945be1
Kabir Singh <kabirsingh12b@gmail.com>
2026-01-09 02:10:09 +0530

    chore : implemented basic routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,20 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import Login from "../src/components/Login.tsx";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/dashboard",
+    element: <App />,
+  },
+]);
 
 async function enableMocking() {
   if (import.meta.env.DEV) {
@@ -14,7 +28,7 @@ async function enableMocking() {
 enableMocking().then(() => {
   createRoot(document.getElementById("root")!).render(
     <StrictMode>
-      <App />
+      <RouterProvider router={router} />,
     </StrictMode>
   );
 });
